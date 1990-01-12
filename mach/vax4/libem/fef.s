7babff444aa5141ca77ee3daa78d9363abe8f772
ceriel <none@none>
1990-01-12 17:02:31 +0000

    some more fixes

--- mach/vax4/libem/fef.s
+++ mach/vax4/libem/fef.s
@@ -9,28 +9,29 @@
 	movl	(sp)+,r3
 	cmpl	r0,$4
 	bneq	I1
-	cvtfd	(sp)+,r0
+	cvtfd	(sp),r0
 	jsb	Ihulp
-	cvtdf	r0,-(sp)
+	cvtdf	r0,(sp)
+	movl	r2,-(sp)
 	jmp	(r3)
 I1:
 	cmpl	r0,$8
 	bneq	I2
-	movd	(sp)+,r0
+	movd	(sp),r0
 	jsb	Ihulp
-	movd	r0,-(sp)
+	movd	r0,(sp)
+	movl	r2,-(sp)
 	jmp	(r3)
 I2:
 	pushl	$EILLINS
 	jmp	.fat
 Ihulp:
-	movl	(sp)+,r2
 	tstd	r0
 	bneq	1f
-	clrl	-(sp)
-	jmp	(r2)
+	clrl	r2
+	rsb
 1:
-	extzv	$7,$8,r0,-(sp)
-	subl2	$128,(sp)
+	extzv	$7,$8,r0,r2
+	subl2	$128,r2
 	insv	$128,$7,$8,r0
-	jmp	(r2)
+	rsb
