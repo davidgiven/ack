f815a342e7b038f07e918255e299548857f68f99
ceriel <none@none>
1990-01-22 14:25:10 +0000

    Use .limhp

--- mach/sun2/libsys/sbrk.s
+++ mach/sun2/libsys/sbrk.s
@@ -7,17 +7,17 @@ _sbrk:
 	move.l	#~3,d1
 	and.l	d1,d0
 	move.l	d0,a0
-	move.l	curbrk,d0
+	move.l	.limhp,d0
 	add.l	#3,d0
 	and.l	d1,d0
-	move.l	d0,curbrk
+	move.l	d0,.limhp
 	add.l	d0,a0
 	move.l	a0,4(sp)
 	pea	17	! not SYS_sbrk
 	trap	#0
 	bcs	1f
-	move.l	curbrk,d0
-	move.l	4(sp),curbrk
+	move.l	.limhp,d0
+	move.l	4(sp),.limhp
 	rts
 1:
 	jmp	cerror
@@ -31,9 +31,6 @@ _brk:
 	pea	17
 	trap	#0
 	bcs	1b
-	move.l	4(sp),curbrk
+	move.l	4(sp),.limhp
 	clr.l	d0
 	rts
-
-.sect .data
-curbrk:	.data4 endbss
--- mach/sun3/libsys/_sbrk.s
+++ mach/sun3/libsys/_sbrk.s
@@ -7,17 +7,17 @@ __sbrk:
 	move.l	#~3,d1
 	and.l	d1,d0
 	move.l	d0,a0
-	move.l	(curbrk),d0
+	move.l	(.limhp),d0
 	add.l	#3,d0
 	and.l	d1,d0
-	move.l	d0,(curbrk)
+	move.l	d0,(.limhp)
 	add.l	d0,a0
 	move.l	a0,(4,sp)
 	pea	(17)	! not SYS_sbrk
 	trap	#0
 	bcs	1f
-	move.l	(curbrk),d0
-	move.l	(4,sp),(curbrk)
+	move.l	(.limhp),d0
+	move.l	(4,sp),(.limhp)
 	rts
 1:
 	jmp	(cerror)
@@ -31,11 +31,8 @@ __brk:
 	pea	(17)
 	trap	#0
 	bcs	1f
-	move.l	(4,sp),(curbrk)
+	move.l	(4,sp),(.limhp)
 	clr.l	d0
 	rts
 1:
 	jmp	(cerror)
-
-.sect .data
-curbrk:	.data4 endbss
