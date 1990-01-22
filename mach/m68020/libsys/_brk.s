7e34a4208893cac91eb992a63ee29f0aa8e1e75a
ceriel <none@none>
1990-01-22 15:03:13 +0000

    Use .limhp

--- mach/m68020/libsys/_brk.s
+++ mach/m68020/libsys/_brk.s
@@ -7,19 +7,19 @@
 .sect .data
 .sect .bss
 .sect .text
-__sbrk:		move.l (.reghp),d0
+__sbrk:		move.l (.limhp),d0
 		add.l  d0,(4,sp)
 		move.l #0x11,d0
 		trap #0
-		bcs lcerror
-		move.l (.reghp),d0
+		bcs Icerror
+		move.l (.limhp),d0
 		move.l d0,a0
-		move.l (4,sp),(.reghp)
+		move.l (4,sp),(.limhp)
 		rts
 lcerror:	jmp (cerror)
 __brk:		move.l #0x11,d0
 		trap #0
 		bcs lcerror
-		move.l (4,sp),(.reghp)
+		move.l (4,sp),(.limhp)
 		clr.l d0
 		rts
