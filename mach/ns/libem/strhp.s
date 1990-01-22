a42999afab9d0b8e349e7b432dd0ae3e691454a7
ceriel <none@none>
1990-01-22 16:25:52 +0000

    Added strhp.s

---
+++ mach/ns/libem/strhp.s
@@ -0,0 +1,25 @@
+#include "em_abs.h"
+.sect .text; .sect .rom; .sect .data; .sect .bss; .sect .text
+.define  .strhp
+
+        ! $Header$
+
+
+.strhp:
+	movd    tos,r7
+	movd    tos,r0
+	movd	r7,tos
+	movd    r0,@.reghp
+	cmpd    r0,@.limhp
+	blo	I2
+	addd    02000,r0
+	bicd    0777,r0
+	movd	r0,tos
+	jsr	@_brk
+	adjspd	-4
+	cmpqd	0,r0
+	beq	I2
+	movd	EHEAP,tos
+	jsr     @.trp
+I2:
+	ret	0
