b3d5eaef0117d00d088734a281f8eed81c6c6e7b
ceriel <none@none>
1990-01-30 18:33:54 +0000

    remove comments in macro texts completely

--- util/cpp/domacro.c
+++ util/cpp/domacro.c
@@ -638,7 +638,7 @@ get_text(formals, length)
 			LoadChar(c);
 			if (c == '*') {
 				skipcomment();
-				text[pos++] = ' ';
+				/* text[pos++] = ' '; ??? Why ??? */
 				LoadChar(c);
 			}
 			else
