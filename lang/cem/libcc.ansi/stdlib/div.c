11c9808d7ebd71ad15fecd63aba74187ce447edd
eck <none@none>
1990-03-01 16:32:22 +0000

    fixed div() and ldiv()

--- lang/cem/libcc.ansi/stdlib/div.c
+++ lang/cem/libcc.ansi/stdlib/div.c
@@ -6,12 +6,26 @@
 
 #include	<stdlib.h>
 
+static int tmp = -1;
+
 div_t
 div(register int numer, register int denom)
 {
 	div_t r;
 
-	r.quot = numer / denom;
-	r.rem = numer % denom;
+	/* The assignment of tmp should not be optimized !! */
+	if (tmp == -1)
+		tmp = (tmp / 2 == 0);
+	}
+	if (numer == 0) {
+		r.quot = numer / denom;		/* might trap if denom == 0 */
+		r.rem = numer % denom;
+	} else if ( !tmp && ((numer < 0) != (denom < 0))) {
+		r.quot = (numer / denom) + 1;
+		r.rem = numer - (numer / denom + 1) * denom
+	} else {
+		r.quot = numer / denom;
+		r.rem = numer % denom;
+	}
 	return r;
 }
--- lang/cem/libcc.ansi/stdlib/ldiv.c
+++ lang/cem/libcc.ansi/stdlib/ldiv.c
@@ -6,12 +6,26 @@
 
 #include	<stdlib.h>
 
+static long tmp = -1;
+
 ldiv_t
 ldiv(register long numer, register long denom)
 {
 	ldiv_t r;
 
-	r.quot = numer / denom;
-	r.rem = numer % denom;
+	/* The assignment of tmp should not be optimized !! */
+	if (tmp == -1)
+		tmp = (tmp / 2 == 0);
+	}
+	if (numer == 0) {
+		r.quot = numer / denom;		/* might trap if denom == 0 */
+		r.rem = numer % denom;
+	} else if ( !tmp && ((numer < 0) != (denom < 0))) {
+		r.quot = (numer / denom) + 1;
+		r.rem = numer - (numer / denom + 1) * denom
+	} else {
+		r.quot = numer / denom;
+		r.rem = numer % denom;
+	}
 	return r;
 }
