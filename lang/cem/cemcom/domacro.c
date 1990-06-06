4d7ba754696115f259f61cf7c74c785b27ee27c5
ceriel <none@none>
1990-06-06 15:35:33 +0000

    removed useless #ifdef

--- lang/cem/cemcom/domacro.c
+++ lang/cem/cemcom/domacro.c
@@ -263,9 +263,7 @@ do_include()
 			fatal("cannot open include file \"%s\"", filenm);
 		}
 		else {
-#ifndef NOPP
 			add_dependency(result);
-#endif  NOPP
 			WorkingDir = getwdir(result);
 			File_Inserted = 1;
 			FileName = result;
