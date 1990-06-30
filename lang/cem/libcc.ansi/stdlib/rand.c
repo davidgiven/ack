c6a11d1f620ba8ceb675db645d5b3435aaa95f26
ceriel <none@none>
1990-06-30 20:02:45 +0000

    rand() did not work on 2-byte machines

--- lang/cem/libcc.ansi/stdlib/rand.c
+++ lang/cem/libcc.ansi/stdlib/rand.c
@@ -11,7 +11,7 @@ static unsigned long int next = 1;
 int rand(void)
 {
 	next = next * 1103515245 + 12345;
-	return (unsigned int)(next/(2 * (RAND_MAX +1)) % (RAND_MAX+1));
+	return (unsigned int)(next/(2 * (RAND_MAX +1L)) % (RAND_MAX+1L));
 }
 
 void srand(unsigned int seed)
