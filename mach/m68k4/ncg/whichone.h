78681448e4f12de7421d706f2f0a7fb689e3c44b
ceriel <none@none>
1990-08-20 15:00:15 +0000

    new table (merged with m68k2)

--- mach/m68k4/ncg/whichone.h
+++ mach/m68k4/ncg/whichone.h
@@ -3,6 +3,6 @@
  * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
  * See the copyright notice in the ACK home directory, in the file "Copyright".
  */
-#ifndef TBL68000
-#define TBL68000
-#endif
+#define WORD_SIZE	4	/* should be 2 or 4 */
+#define TBL68000	1	/* should be TBL68000 or TBL68020 */
+/* define TBL68881	1	/* define to use floating point processor */
