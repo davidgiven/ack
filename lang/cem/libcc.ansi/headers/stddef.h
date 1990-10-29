68f19d7335676e76404528033463675a7995e6c4
eck <none@none>
1990-10-29 13:52:18 +0000

    changed offsetof(), so it will not generate a warning on 2-4 machines

--- lang/cem/libcc.ansi/headers/stddef.h
+++ lang/cem/libcc.ansi/headers/stddef.h
@@ -11,7 +11,8 @@
 
 #define	NULL		((void *)0)
 
-#define	offsetof(type, ident)		((size_t) &(((type *)0)->ident))
+#define	offsetof(type, ident)	((size_t) (unsigned long) (char *)(&((type *)0)->ident))
+
 
 #if	_EM_PSIZE == _EM_WSIZE
 typedef int	ptrdiff_t;	/* result of substracting two pointers */
