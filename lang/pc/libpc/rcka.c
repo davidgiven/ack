f0fe57f807dae513fd27314f13faef9f84c8ad11
ceriel <none@none>
1990-11-06 13:16:57 +0000

    Added Copyright notice

--- lang/pc/libpc/rcka.c
+++ lang/pc/libpc/rcka.c
@@ -1,3 +1,9 @@
+/* $Header$ */
+/*
+ * (c) copyright 1990 by the Vrije Universiteit, Amsterdam, The Netherlands.
+ * See the copyright notice in the ACK home directory, in the file "Copyright".
+ */
+
 /* Author: Hans van Eck */
 
 #include	<em_abs.h>
