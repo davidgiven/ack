8357c43aeeeb836703978ebac3d58af2f361068d
eck <none@none>
1990-11-08 12:01:52 +0000

    corrected type of *_EPSILON values

--- lang/cem/libcc.ansi/headers/float.h
+++ lang/cem/libcc.ansi/headers/float.h
@@ -8,7 +8,7 @@
 
 #if	defined(vax) || defined(pdp)
 #define	FLT_DIG			6
-#define	FLT_EPSILON		5.96046448e-08
+#define	FLT_EPSILON		5.96046448e-08F
 #define	FLT_MANT_DIG		8
 #define	FLT_MAX			1.70141173e+38F
 #define	FLT_MAX_10_EXP		38
@@ -28,7 +28,7 @@
 #define	DBL_MIN_EXP		(-127)
 
 #define	LDBL_DIG		16
-#define	LDBL_EPSILON		1.38777878078144568e-17
+#define	LDBL_EPSILON		1.38777878078144568e-17L
 #define	LDBL_MANT_DIG		8
 #define	LDBL_MAX		1.70141183460469229e+38L
 #define	LDBL_MAX_10_EXP		38
@@ -42,7 +42,7 @@
 
 #else		/* IEEE format */
 #define	FLT_DIG			6
-#define	FLT_EPSILON		1.19209290e-07
+#define	FLT_EPSILON		1.19209290e-07F
 #define	FLT_MANT_DIG		24
 #define	FLT_MAX			3.40282347e+38F
 #define	FLT_MAX_10_EXP		38
@@ -62,7 +62,7 @@
 #define	DBL_MIN_EXP		(-1021)
 
 #define	LDBL_DIG		15
-#define	LDBL_EPSILON		2.2204460492503131e-16
+#define	LDBL_EPSILON		2.2204460492503131e-16L
 #define	LDBL_MANT_DIG		53
 #define	LDBL_MAX		1.7976931348623157e+308L
 #define	LDBL_MAX_10_EXP		308
