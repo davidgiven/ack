974463cbd4ac09f0b24ef491e2dce1edd9ccfda6
ceriel <none@none>
1990-12-19 12:42:21 +0000

    Fixed stat interface

--- mach/mantra/libsys/_fstat.s
+++ mach/mantra/libsys/_fstat.s
@@ -5,7 +5,22 @@
 .sect .data
 .sect .bss
 .sect .text
-__fstat:		move.l	#0x1C,d0
+__fstat:	move.l	#0x1C,d0
 		move.l	4(sp),a0
 		move.l	8(sp),d1
-		jmp callc
+		jsr callc
+		tst.l d0
+		blt 1f
+		move.l 4(sp),a0
+		add.l #30,a0
+		lea 2(a0),a1
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+1:
+		rts
--- mach/mantra/libsys/stat.s
+++ mach/mantra/libsys/stat.s
@@ -8,4 +8,19 @@
 _stat:		move.l #0x12,d0
 		move.l 4(sp),a0
 		move.l 8(sp),d1
-		jmp callc
+		jsr callc
+		tst.l d0
+		blt 1f
+		move.l 4(sp),a0
+		add.l #30,a0
+		lea 2(a0),a1
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+		move.w -(a0),-(a1)
+1:
+		rts
