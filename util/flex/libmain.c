29b2e6e3d4e9cf97749bcba30fcaea0a93487804
ceriel <none@none>
1991-01-16 16:07:50 +0000

    initial version

---
+++ util/flex/libmain.c
@@ -0,0 +1,13 @@
+/* libmain - flex run-time support library "main" function */
+
+/* $Header$ */
+
+extern int yylex();
+
+int main( argc, argv )
+int argc;
+char *argv[];
+
+    {
+    return yylex();
+    }
