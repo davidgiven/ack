2094dcecfee36f721651aa6748f8496e2a119e85
ceriel <none@none>
1991-01-22 12:43:40 +0000

    assembling of (xxx,a6) addressing mode was wrong for large values of xxx

--- mach/sun3/ce/mach.c
+++ mach/sun3/ce/mach.c
@@ -1,7 +1,6 @@
 #define CODE_EXPANDER
 #include "mach.h"
 #include <back.h>
-#include <out.h>
 #include <system.h>
 
 
@@ -28,3 +27,44 @@ char *filename;
 */
 
 #include <con_float>
+
+__instr_code(code, reg, off)
+{
+  if (off <= 32767 & off >= -32768) {
+	text2(code|0x28|reg);
+	text2(off);
+	return;
+  }
+  text2(code|0x30|reg);
+  text2(0x0170);
+  text4(off);
+}
+
+__move_X(code, reg, off)
+{
+  if (off <= 32767 & off >= -32768) {
+	text2(code|(reg<<9)|0x140);
+	text2(off);
+	return;
+  }
+  text2(code|(reg<<9)|0x180);
+  text2(0x0170);
+  text4(off);
+}
+
+__moveXX(code, srcreg, srcoff, dstreg, dstoff)
+{
+  if (srcoff <= 32767 && srcoff >= -32768) {
+	__move_X(code|0x28|srcreg, dstreg, dstoff);
+	return;
+  }
+  if (dstoff <= 32767 && dstoff >= -32768) {
+	__instr_code(code|0x140|(dstreg<<9), srcreg, srcoff);
+	return;
+  }
+  text2(code|(dstreg<<9)|srcreg|0x180|0x30);
+  text2(0x0170);
+  text4(srcoff);
+  text2(0x0170);
+  text4(dstoff);
+}
