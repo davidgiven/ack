ac966f41ebc5a07c5ba469fb0b7b0c88de46b542
ceriel <none@none>
1991-02-01 15:38:19 +0000

    Added end library and floating point processor support

--- mach/m68k4/libem/end.s
+++ mach/m68k4/libem/end.s
@@ -1,16 +1,7 @@
-.define	endtext,enddata,endbss,_etext,_edata,_end
 .sect .text
 .sect .rom
 .sect .data
 .sect .bss
-.sect .end ! only for declaration of _end and endbss.
-
-	.sect .text
-endtext:
-_etext:
-	.sect .data
-enddata:
-_edata:
-	.sect .end
+.define	_end
+.sect .end ! only for declaration of _end, __end and endbss.
 _end:
-endbss:
