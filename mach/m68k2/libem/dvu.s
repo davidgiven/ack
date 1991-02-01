0f4e675b50be005efa684e23a22257a4b4431f36
ceriel <none@none>
1991-02-01 15:09:58 +0000

    Added end library and floating point processor support

--- mach/m68k2/libem/dvu.s
+++ mach/m68k2/libem/dvu.s
@@ -5,34 +5,77 @@
 .sect .bss
 
  ! unsigned long divide
+ !-----------------------------------------------------------------------------
+ ! rewritten by Kai-Uwe Bloem (i5110401@dbstu1.bitnet) for speed.
+ !   #1  01/12/90  initial revision. Minor reduce of shift operations.
+ !   #2  03/07/90  use 68000 divu instruction whereever possible. This change
+ !		   makes #1 superflous. (derived from my GNU division routine)
+ !-----------------------------------------------------------------------------
+ ! Some common cases can be handled in a special, much faster way :
+ !      1) divisor = 0
+ !          => cause trap, then return to user. Result is undefined
+ !      2) dividend < divisor
+ !          => quotient = 0, remainder = dividend
+ !      3) divisor < 0x10000 ( i.e. divisor is only 16 bits wide )
+ !          => quotient and remainder can be calculated quite fast by repeated
+ !             application of 68000 divu operations (ca. 400 cycles)
+ !      4) otherwise (due to #2, #3 dividend, divisor both wider then 16 bits)
+ !          => do slow division by shift and subtract
+ !-----------------------------------------------------------------------------
+
+
  ! register usage:
  ! 	 : d0 divisor
  !         d1 dividend
  ! exit  : d1 quotient
  !         d2 remainder
+
 	.sect .text
 .dvu:
+	move.l	d3,a0		! save d3
 	move.l	(sp)+,a1	! return address
-	move.l	(sp)+,d0
-	move.l	(sp)+,d1
-	move.l	d3,-(sp)	! save d3
-	tst.l	d0
-	bne	0f
-	move.l	(sp)+,d3
-	move.w	#EIDIVZ,-(sp)
-	jsr	.trp
-0:
-	clr.l	d2
-	move.l	#32,d3
-3:
-	lsl.l	#1,d1
-	roxl.l	#1,d2
-	cmp.l	d0,d2
-	blt	4f
-	sub.l	d0,d2
-	add	#1,d1
+	move.l	(sp)+,d0	! divisor
+	move.l	(sp)+,d2	! dividend
+	clr.l	d1		! prepare quotient
+! === case 1: divisor = 0
+	tst.l	d0		! divisor = 0 ?
+	beq	9f		! yes - divide by zero trap
+! === case 2: dividend < divisor
+	cmp.l	d0,d2		! dividend < divisor ?
+	bcs	8f		! yes - division already finished
+! === case 3: divisor <= 0x0ffff
+	cmp.l	#0x0ffff,d0	! is divisor only 16 bits wide ?
+	bhi	2f
+	move.w	d2,d3		! save dividend.l
+	clr.w	d2		! prepare dividend.h for divu operation
+	swap	d2
+	beq	0f		! dividend.h is all zero, no divu necessary
+	divu	d0,d2
+0:	move.w	d2,d1		! save quotient.h
+	swap	d1
+	move.w	d3,d2		! divide dividend.l
+	divu	d0,d2		! (d2.h = remainder of prev divu)
+	move.w	d2,d1		! save qoutient.l
+	clr.w	d2		! get remainder
+	swap	d2
+	bra	8f
+! === case 4: divisor and dividend both > 0x0ffff
+2:
+	move	#32-1,d3	! loop count
 4:
-	sub	#1,d3
-	bgt	3b
-	move.l	(sp)+,d3
+	lsl.l	#1,d2		! shift dividend ...
+	roxl.l	#1,d1		!  ... into d1
+	cmp.l	d0,d1		! compare with divisor
+	bcs	5f
+	sub.l	d0,d1		! bigger, subtract divisor
+	add	#1,d2		! note subtraction in result
+5:
+	dbra	d3,4b
+	exg	d1,d2		! get results in the correct registers
+8:
+	move.l	a0,d3		! restore d3
 	jmp	(a1)
+
+EIDIVZ	= 6
+9:	move.w	#EIDIVZ,-(sp)
+	jsr	.trp
---
+++ mach/m68k2/libem/fp68881.s
@@ -0,0 +1,352 @@
+.define .adf4, .adf8, .sbf4, .sbf8, .mlf4, .mlf8, .dvf4, .dvf8
+.define .ngf4, .ngf8, .fif4, .fif8, .fef4, .fef8
+.define .cif4, .cif8, .cuf4, .cuf8, .cfi, .cfu, .cff4, .cff8
+.define .cmf4, .cmf8
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+
+!	$Header$
+
+!	Implement interface to floating point package for M68881
+
+	.sect .text
+.adf4:
+	move.l	(sp)+,a0
+	fmove.s (sp),fp0
+	fadd.s	4(sp),fp0
+	fmove.s	fp0,4(sp)
+	jmp	(a0)
+
+.adf8:
+	move.l	(sp)+,a0
+	fmove.d (sp),fp0
+	fadd.d	8(sp),fp0
+	fmove.d	fp0,8(sp)
+	jmp	(a0)
+
+.sbf4:
+	move.l	(sp)+,a0
+	fmove.s (sp),fp0
+	fmove.s	4(sp),fp1
+	fsub	fp0,fp1
+	fmove.s	fp1,4(sp)
+	jmp	(a0)
+
+.sbf8:
+	move.l	(sp)+,a0
+	fmove.d (sp),fp0
+	fmove.d	8(sp),fp1
+	fsub	fp0,fp1
+	fmove.d	fp1,8(sp)
+	jmp	(a0)
+
+.mlf4:
+	move.l	(sp)+,a0
+	fmove.s (sp),fp0
+	fmul.s	4(sp),fp0
+	fmove.s	fp0,4(sp)
+	jmp	(a0)
+
+.mlf8:
+	move.l	(sp)+,a0
+	fmove.d (sp),fp0
+	fmul.d	8(sp),fp0
+	fmove.d	fp0,8(sp)
+	jmp	(a0)
+
+.dvf4:
+	move.l	(sp)+,a0
+	fmove.s (sp),fp0
+	fmove.s	4(sp),fp1
+	fdiv	fp0,fp1
+	fmove.s	fp1,4(sp)
+	jmp	(a0)
+
+.dvf8:
+	move.l	(sp)+,a0
+	fmove.d (sp),fp0
+	fmove.d	8(sp),fp1
+	fdiv	fp0,fp1
+	fmove.d	fp1,8(sp)
+	jmp	(a0)
+
+.ngf4:
+	fmove.s	4(sp),fp0
+	fneg	fp0
+	fmove.s	fp0,4(sp)
+	rts
+
+.ngf8:
+	fmove.d	4(sp),fp0
+	fneg	fp0
+	fmove.d	fp0,4(sp)
+	rts
+
+.fif4:
+	move.l	(sp)+,a0
+	move.l	(sp),a1
+	fmove.s	4(sp),fp0
+	fmove.s	8(sp),fp1
+	fmul	fp0,fp1
+	fintrz	fp1,fp0
+	fsub	fp0,fp1
+	fmove.s	fp1,4(a1)
+	fmove.s	fp0,(a1)
+	jmp	(a0)
+
+.fif8:
+	move.l	(sp)+,a0
+	move.l	(sp),a1
+	fmove.d	4(sp),fp0
+	fmove.d	12(sp),fp1
+	fmul	fp0,fp1
+	fintrz	fp1,fp0
+	fsub	fp0,fp1
+	fmove.d	fp1,8(a1)
+	fmove.d	fp0,(a1)
+	jmp	(a0)
+
+.fef4:
+	move.l	(sp)+,a0
+	move.l	(sp),a1
+	fmove.s	4(sp),fp0
+	fgetexp	fp0,fp1
+	fmove.l	fpsr,d0
+	and.l	#0x2000,d0	! set if Infinity
+	beq	1f
+	move.w	#129,(a1)
+	fmove.s	4(sp),fp0
+	fblt	2f
+	move.l	#0x3f000000,2(a1)
+	jmp	(a0)
+2:
+	move.l	#0xbf000000,2(a1)
+	jmp	(a0)
+1:
+	fmove.l	fp1,d0
+	add.l	#1,d0
+	fgetman	fp0
+	fbne	1f
+	clr.l	d0
+	bra	2f
+1:
+	fmove.l	#2,fp1
+	fdiv	fp1,fp0
+2:
+	fmove.s	fp0,2(a1)
+	move.w	d0,(a1)
+	jmp	(a0)
+
+.fef8:
+	move.l	(sp)+,a0
+	move.l	(sp),a1
+	fmove.d	4(sp),fp0
+	fgetexp	fp0,fp1
+	fmove.l	fpsr,d0
+	and.l	#0x2000,d0	! set if Infinity
+	beq	1f
+	move.w	#1025,(a1)
+	fmove.d	4(sp),fp0
+	fblt	2f
+	move.l	#0x3fe00000,2(a1)
+	clr.l	6(a1)
+	jmp	(a0)
+2:
+	move.l	#0xbfe00000,2(a1)
+	clr.l	6(a1)
+	jmp	(a0)
+1:
+	fmove.l	fp1,d0
+	add.l	#1,d0
+	fgetman	fp0
+	fbne	1f
+	clr.l	d0
+	bra	2f
+1:
+	fmove.l	#2,fp1
+	fdiv	fp1,fp0
+2:
+	fmove.d	fp0,2(a1)
+	move.w	d0,(a1)
+	jmp	(a0)
+
+.cif4:
+	move.l	(sp)+,a0
+	cmp.w	#2,(sp)
+	bne	1f
+	fmove.w	2(sp),fp0
+	fmove.s	fp0,(sp)
+	jmp	(a0)
+1:
+	fmove.l	2(sp),fp0
+	fmove.s	fp0,2(sp)
+	jmp	(a0)
+
+.cif8:
+	move.l	(sp)+,a0
+	cmp.w	#2,(sp)
+	bne	1f
+	fmove.w	2(sp),fp0
+	fmove.d	fp0,(sp)
+	jmp	(a0)
+1:
+	fmove.l	2(sp),fp0
+	fmove.d	fp0,(sp)
+	jmp	(a0)
+
+.cuf4:
+	move.l	(sp)+,a0
+	cmp.w	#2,(sp)
+	bne	2f
+	fmove.w	2(sp),fp0
+	tst.w	2(sp)
+	bge	1f
+	fadd.l	#65536,fp0
+1:
+	fmove.s	fp0,(sp)
+	jmp	(a0)
+2:
+	fmove.l	2(sp),fp0
+	tst.l	2(sp)
+	bge	1f
+	fsub.l	#-2147483648,fp0
+	fsub.l	#-2147483648,fp0
+1:
+	fmove.s	fp0,2(sp)
+	jmp	(a0)
+
+.cuf8:
+	move.l	(sp)+,a0
+	move.w	(sp),d0
+	cmp.w	#2,d0
+	bne	2f
+	fmove.w	2(sp),fp0
+	tst.w	2(sp)
+	bge	1f
+	fadd.l	#65536,fp0
+	bra	1f
+2:
+	fmove.l	2(sp),fp0
+	tst.l	2(sp)
+	bge	1f
+	fsub.l	#-2147483648,fp0
+	fsub.l	#-2147483648,fp0
+1:
+	fmove.d	fp0,(sp)
+	jmp	(a0)
+
+.cfi:
+	move.l	(sp)+,a0
+	move.w	(sp),d1
+	move.w	2(sp),d0
+	cmp.w	#4,d0
+	bne	1f
+	fmove.s	4(sp),fp0
+	bra	2f
+1:
+	fmove.d	4(sp),fp0
+	add.l	#4,sp
+2:
+	cmp.w	#2,d1
+	bne	1f
+	fmove.w	fp0,6(sp)
+	bra	2f
+1:
+	fmove.l	fp0,4(sp)
+2:
+	cmp.w	#4,d0
+	beq	1f
+	sub.l	#4,sp
+1:
+	jmp	(a0)
+
+.cfu:
+	move.l	(sp)+,a0
+	move.w	(sp),d1
+	move.w	2(sp),d2
+	cmp.w	#4,d2
+	bne	1f
+	fmove.s	4(sp),fp0
+	fabs	fp0
+	cmp.l	#0x4f000000,4(sp)
+	bge	2f
+	fintrz	fp0,fp0
+	fmove.l	fp0,d0
+	bra	3f
+2:
+	fadd.l	#-2147483648,fp0
+	fintrz	fp0,fp0
+	fmove.l	fp0,d0
+	bchg	#31,d0
+	bra	3f
+1:
+	fmove.d	4(sp),fp0
+	add.l	#4,sp
+	fabs	fp0
+	cmp.l	#0x41e00000,(sp)
+	bge	1f
+	fintrz	fp0,fp0
+	fmove.l	fp0,d0
+	bra	3f
+1:
+	fadd.l	#-2147483648,fp0
+	fintrz	fp0,fp0
+	fmove.l	fp0,d0
+	bchg	#31,d0
+3:
+	cmp.w	#2,d1
+	bne	1f
+	move.w	d0,6(sp)
+	bra	2f
+1:
+	move.l	d0,4(sp)
+2:
+	cmp.w	#4,d2
+	beq	1f
+	sub.l	#4,sp
+1:
+	jmp	(a0)
+
+.cff4:
+	move.l	(sp)+,a0
+	fmove.d	(sp),fp0
+	fmove.s	fp0,4(sp)
+	jmp	(a0)
+
+.cff8:
+	move.l	(sp)+,a0
+	fmove.s	(sp),fp0
+	fmove.d	fp0,(sp)
+	jmp	(a0)
+
+.cmf4:
+	move.l	(sp)+,a0
+	clr.l	d0
+	fmove.s	(sp),fp0
+	fmove.s	4(sp),fp1
+	fcmp	fp0,fp1
+	fbeq	2f
+	fblt	1f
+	add.l	#1,d0
+	jmp	(a0)
+1:
+	sub.l	#1,d0
+2:
+	jmp	(a0)
+
+.cmf8:
+	move.l	(sp)+,a0
+	clr.l	d0
+	fmove.d	(sp),fp0
+	fmove.d	8(sp),fp1
+	fcmp	fp0,fp1
+	fbeq	2f
+	fblt	1f
+	add.l	#1,d0
+	jmp	(a0)
+1:
+	sub.l	#1,d0
+2:
+	jmp	(a0)
