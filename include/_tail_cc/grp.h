9ede34412cfe23c491ce3620f9f6d5dddf708d82
ceriel <none@none>
1991-10-04 14:34:29 +0000

    Added

---
+++ include/_tail_cc/grp.h
@@ -0,0 +1,21 @@
+/* $Header$ */
+/*
+ * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
+ * See the copyright notice in the ACK home directory, in the file "Copyright".
+ */
+
+#ifndef _GRP_H
+#define _GRP_H
+
+struct	group { 
+  char *gr_name;		/* the name of the group */
+  char *gr_passwd;		/* the group passwd */
+  gid_t gr_gid;			/* the numerical group ID */
+  char **gr_mem;		/* a vector of pointers to the members */
+};
+
+extern struct group *getgrgid();
+extern struct group *getgrnam();
+extern struct group *getgrent();
+
+#endif /* _GRP_H */
---
+++ include/_tail_cc/pwd.h
@@ -0,0 +1,26 @@
+/* $Header$ */
+/*
+ * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
+ * See the copyright notice in the ACK home directory, in the file "Copyright".
+ */
+
+#ifndef _PWD_H
+#define _PWD_H
+
+struct passwd {
+  char *pw_name;		/* login name */
+  uid_t pw_uid;			/* uid corresponding to the name */
+  gid_t pw_gid;			/* gid corresponding to the name */
+  char *pw_dir;			/* user's home directory */
+  char *pw_shell;		/* name of the user's shell */
+
+  /* The following members are not defined by POSIX. */
+  char *pw_passwd;		/* password information */
+  char *pw_gecos;		/* just in case you have a GE 645 around */
+};
+
+extern struct passwd *getpwnam();
+extern struct passwd *getpwuid();
+extern struct passwd *getpwent();
+
+#endif /* _PWD_H */
