dc44eba0629029905aa76c6598bd65dd6fd7188d
ceriel <none@none>
1991-12-16 13:45:18 +0000

    Do not use '#undef aap xxx'; it is not allowed for ANSI C

--- emtest/test.h
+++ emtest/test.h
@@ -3,6 +3,6 @@
  * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
  * See the copyright notice in the ACK home directory, in the file "Copyright".
  */
-#undef W2S	4
-#undef FS 	4
-#undef F2S	8
+/*#define W2S	4	/* double word size */
+/*#define FS 	4	/* float size */
+/*#define F2S	8	/* double size */
