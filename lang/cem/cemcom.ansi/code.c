b4aef1eb5ec0950b81d33016502dc785676a0c9e
ceriel <none@none>
1993-05-12 08:33:03 +0000

    Fixed error: did not compile

--- lang/cem/cemcom.ansi/code.c
+++ lang/cem/cemcom.ansi/code.c
@@ -111,7 +111,7 @@ code_string(val, len)
 	int len;
 {
 	register struct string_cst *sc = new_string_cst();
-	label lbl = data_label();
+	label dlb = data_label();
 
 	C_ina_dlb(dlb);
 	sc->next = str_list;
