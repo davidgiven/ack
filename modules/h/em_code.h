1da6506ba71747c3d80db1cbe2c93d49da523408
ceriel <none@none>
1993-11-05 15:02:57 +0000

    Added prototypes

--- modules/h/em_code.h
+++ modules/h/em_code.h
@@ -5,6 +5,25 @@
  */
 
 #include "em_mesX.h"
+#include "ansi.h"
+
+_PROTOTYPE(void C_ms_com, (char *));
+_PROTOTYPE(void C_ms_ego, (int, arith, arith, int));
+_PROTOTYPE(void C_ms_emx, ( arith, arith));
+_PROTOTYPE(void C_ms_err, (void));
+_PROTOTYPE(void C_ms_flt, (void));
+_PROTOTYPE(void C_ms_gto, (void));
+_PROTOTYPE(void C_ms_opt, (void));
+_PROTOTYPE(void C_ms_par, (arith));
+_PROTOTYPE(void C_ms_reg, (arith, arith, int, int));
+_PROTOTYPE(void C_ms_src, (int, char *));
+_PROTOTYPE(void C_ms_stb_cst, (char *, int, int, arith));
+_PROTOTYPE(void C_ms_stb_dlb, (char *, int, int, label, arith));
+_PROTOTYPE(void C_ms_stb_dnam, (char *, int, int, char *, arith));
+_PROTOTYPE(void C_ms_stb_ilb, (char *, int, int, label));
+_PROTOTYPE(void C_ms_stb_pnam, (char *, int, int, char *));
+_PROTOTYPE(void C_ms_std, (char *, int, int));
+
 #ifdef PEEPHOLE
 #include "em_codeO.h"
 #include "emO_code.h"
