d2200811981fee2608e3a63787ae46f8b132c35b
dtrg <none@none>
2006-07-22 00:45:51 +0000

    Changed to declare symbols as extern rather than just mentioning them without qualification.

--- util/ego/sr/sr_xform.h
+++ util/ego/sr/sr_xform.h
@@ -11,12 +11,12 @@
 
 
 
-line_p move_pointer(); /* (offset tmp; int dir ) */
+extern line_p move_pointer(); /* (offset tmp; int dir ) */
 	/* Generate EM code to load/store a pointer variable
 	 * onto/from the stack, depending on dir(ection).
 	 * We accept all kinds of pointer sizes.
 	 */
-make_header() ;	/* (loop_p lp) */
+extern make_header() ;	/* (loop_p lp) */
 	/* Make sure that the loop has a header block, i.e. a block
 	 * has the loop entry block as its only successor and
 	 * that dominates the loop entry block.
