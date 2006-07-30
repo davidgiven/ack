1efe7422d375a97917944b786a00da4d072c0218
dtrg <none@none>
2006-07-30 23:44:57 +0000

    Added rules for building things with the ack itself.

---
+++ first/ack.pm
@@ -0,0 +1,33 @@
+-- $Source$
+-- $State$
+
+-- Provides rules for building things with the half-built ACK itself.
+
+ACKBUILDFLAGS = {"-m%ARCH%", "%OPTIMISATION%"}
+ACKDEFINES = EMPTY
+ACKINCLUDES = EMPTY
+
+ackfile = simple_with_clike_dependencies {
+	class = "ackfile",
+	CINCLUDES = {REDIRECT, "ACKINCLUDES"},
+	command = {
+		"%BINDIR%bin/ack %ACKBUILDFLAGS% %ACKINCLUDES% %ACKDEFINES% -c -o %out% %in%"
+	},
+	outputs = {"%U%-%I%.o"},
+}
+
+ackprogram = simple {
+	class = "ackprogram",
+	command = {
+		"%BINDIR%bin/ack %ACKBUILDFLAGS% -o %out% %in%"
+	},
+	outputs = {"%U%-%I%"},
+}
+
+acklibrary = simple {
+	class = "acklibrary",
+	command = {
+		"%RM% %out% && %BINDIR%bin/aal cr %out% %in%"
+	},
+	outputs = {"%U%-%I%.a"},
+}
