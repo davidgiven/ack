58e5e12eadb9f3e70b99859a15919dc13b3da081
cvs2hg <none@none>
2007-04-29 23:09:25 +0000

    fixup commit for tag 'release-6-0-pre-3'

--- util/cpp/Version.c
+++
@@ -1,8 +0,0 @@
-/* $Header$ */
-/*
- * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
- * See the copyright notice in the ACK home directory, in the file "Copyright".
- */
-#ifndef	lint
-static char Version[] = "ACK C preprocessor Version 1.3";
-#endif	lint
