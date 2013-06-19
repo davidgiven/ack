55be35a68a9754a4c5c34ae2ff18ff1005b75d84
David Given <dg@cowlark.com>
2013-06-20 00:15:14 +0100

    Add a stub malloc.h.
    --HG--
    branch : dtrg-videocore
    rename : lang/cem/libcc.ansi/headers/stdlib.h => lang/cem/libcc.ansi/headers/malloc.h

---
+++ lang/cem/libcc.ansi/headers/malloc.h
@@ -0,0 +1,14 @@
+/*
+ * stdlib.h - standard library
+ *
+ * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
+ * See the copyright notice in the ACK home directory, in the file "Copyright".
+ */
+/* $Id$ */
+
+#ifndef _MALLOC_H
+#define	_MALLOC_H
+
+#include <stdlib.h>
+
+#endif
