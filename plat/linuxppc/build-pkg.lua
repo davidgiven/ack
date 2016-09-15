45a950571d539fc8819820882f17c9eb158fdad6
David Given <dg@cowlark.com>
2016-09-15 23:12:03 +0200

    Mostly add support for the experimental and largely broken linuxppc platform. (Doesn't quite build.)
    --HG--
    branch : dtrg-experimental-powerpc-branch

---
+++ plat/linuxppc/build-pkg.lua
@@ -0,0 +1,25 @@
+include("plat/build.lua")
+
+ackfile {
+	name = "boot",
+	srcs = { "./boot.s" },
+	vars = { plat = "linuxppc" }
+}
+
+build_plat_libs {
+	name = "libs",
+	arch = "powerpc",
+	plat = "linuxppc",
+}
+
+installable {
+	name = "pkg",
+	map = {
+		"+tools",
+		"+libs",
+		"./include+pkg",
+		["$(PLATIND)/linuxppc/boot.o"] = "+boot",
+        ["$(PLATIND)/linuxppc/libsys.a"] = "./libsys+lib",
+	}
+}
+
