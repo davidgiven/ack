03b067e1d59e4663e52489fd3ec78f3a11759dd6
George Koehler <xkernigh@netscape.net>
2016-09-17 23:55:55 -0400

    Add the missing .lar4 and .sar4 for powerpc.
    Inspired by the sparc code (mach/sparc/libem/lar.s).  My powerpc code
    might still have bugs, but it's enough for examples/hilo.mod to work.
    
    May need to 'make clean' or touch a build.lua file, so ackbuilder can
    notice the new lar4.s and sar4.s files and build them.

--- mach/powerpc/libem/aar4.s
+++ mach/powerpc/libem/aar4.s
@@ -13,6 +13,10 @@
 !    r3 = ptr to descriptor
 !    r4 = index
 !    r5 = address of array
+! Yields:
+!    r3 = address of element
+!    r0 = size of element (used by .lar4, .sar4)
+! Preserves r10 for .lar4, .sar4
 
 .define .aar4
 .aar4:
