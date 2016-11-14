5e8babf098ec00a6bafe9a0fd5f9cfb53746af56
David Given <dg@cowlark.com>
2016-11-14 22:12:13 +0100

    Add a basic integer shift test.

---
+++ plat/qemuppc/tests/intshift.c
@@ -0,0 +1,52 @@
+#include "test.h"
+
+/* Constants in globals to defeat constant folding. */
+int one = 1;
+int zero = 0;
+int minusone = -1;
+
+/* Bypasses the CRT, so there's no stdio or BSS initialisation. */
+void _m_a_i_n(void)
+{
+    ASSERT((one     <<zero) == 1);
+    ASSERT((one     <<one)  == 2);
+    ASSERT((minusone<<zero) == -1);
+    ASSERT((minusone<<one)  == -2);
+
+    ASSERT(((unsigned int)one     <<(unsigned int)zero) == 1);
+    ASSERT(((unsigned int)one     <<(unsigned int)one)  == 2);
+    ASSERT(((unsigned int)minusone<<(unsigned int)zero) == -1);
+    ASSERT(((unsigned int)minusone<<(unsigned int)one)  == -2);
+
+    ASSERT((one     >>zero) == 1);
+    ASSERT((one     >>one)  == 0);
+    ASSERT((minusone>>zero) == -1);
+    ASSERT((minusone>>one)  == -1);
+
+    ASSERT(((unsigned int)one     >>(unsigned int)zero) == 1);
+    ASSERT(((unsigned int)one     >>(unsigned int)one)  == 0);
+    ASSERT(((unsigned int)minusone>>(unsigned int)zero) == 0xffffffff);
+    ASSERT(((unsigned int)minusone>>(unsigned int)one)  == 0x7fffffff);
+
+    ASSERT((one     <<0) == 1);
+    ASSERT((one     <<1)  == 2);
+    ASSERT((minusone<<0) == -1);
+    ASSERT((minusone<<1)  == -2);
+
+    ASSERT(((unsigned int)one     <<(unsigned int)0) == 1);
+    ASSERT(((unsigned int)one     <<(unsigned int)1)  == 2);
+    ASSERT(((unsigned int)minusone<<(unsigned int)0) == -1);
+    ASSERT(((unsigned int)minusone<<(unsigned int)1)  == -2);
+
+    ASSERT((one     >>0) == 1);
+    ASSERT((one     >>1)  == 0);
+    ASSERT((minusone>>0) == -1);
+    ASSERT((minusone>>1)  == -1);
+
+    ASSERT(((unsigned int)one     >>(unsigned int)0) == 1);
+    ASSERT(((unsigned int)one     >>(unsigned int)1)  == 0);
+    ASSERT(((unsigned int)minusone>>(unsigned int)0) == 0xffffffff);
+    ASSERT(((unsigned int)minusone>>(unsigned int)1)  == 0x7fffffff);
+
+    finished();
+}
