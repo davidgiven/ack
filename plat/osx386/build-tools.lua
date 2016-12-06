6cbea83f7f969910b97e283297a83979634da190
George Koehler <xkernigh@netscape.net>
2016-12-05 20:13:29 -0500

    Don't share as, ncg, top between Linux and Mac OS X.
    Telling osx386 and osxppc to build and run their own tools, not to
    reuse the tools from linux386 and linuxppc.  This wastes time to build
    identical tools, but it removes some bogus dependencies.  OS X tools
    had wrongly depended on Linux descr files and aelflod; now they don't.
    
    Discussion in https://github.com/davidgiven/ack/pull/23

--- plat/osx386/build-tools.lua
+++ plat/osx386/build-tools.lua
@@ -1,8 +1,21 @@
+include("plat/build.lua")
+
+build_as {
+	name = "as",
+	arch = "i386",
+}
+
+build_ncg {
+	name = "ncg",
+	arch = "i386",
+}
+
 return installable {
 	name = "tools",
 	map = {
+		["$(PLATDEP)/osx386/as"] = "+as",
+		["$(PLATDEP)/osx386/ncg"] = "+ncg",
 		["$(PLATIND)/descr/osx386"] = "./descr",
-		"plat/linux386+tools",
 		"plat/osx/cvmach+pkg",
 		"util/opt+pkg",
 	}
