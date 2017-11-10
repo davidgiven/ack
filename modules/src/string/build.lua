63674674998e71fa7fec27482ad40a26be26211f
George Koehler <xkernigh@netscape.net>
2017-11-09 23:25:17 -0500

    Remove functions that also exist in libc.
    Some of these functions were slightly different from libc:
    
     - This strncpy() didn't pad the buffer with '\0' bytes beyond the end
       of the string; libc does the padding.  This string.3 manual said
       that this strncpy() does "null-padding", but it didn't.
    
     - This strcmp() and strncmp() compared using char (which might be
       signed); libc compares using unsigned char.

--- modules/src/string/build.lua
+++ modules/src/string/build.lua
@@ -1,10 +1,13 @@
 clibrary {
 	name = "lib",
-	srcs = { "./*.c" },
+	srcs = {
+		"./bts2str.c", "./btscat.c", "./btscmp.c",
+		"./btscpy.c", "./btszero.c", "./long2str.c",
+		"./str2bts.c", "./str2long.c", "./strindex.c",
+		"./strrindex.c", "./strzero.c",
+	},
 	deps = {
+		"./ack_string.h",
 		"modules+headers",
-		"./*.h"
 	},
 }
-
-
