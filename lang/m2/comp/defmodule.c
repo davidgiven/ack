davidgiven/ack

commit 893471a42eb4d189cc0ba1dac1864d6f151bccd7

No longer truncate module names at 10 characters when constructing paths; rename some library modules to their full names.

lang/m2/comp/defmodule.c
@@ -17,6 +17,7 @@
 #include <em_arith.h>
 #include <em_label.h>
 #include <stdlib.h>
+#include <astring.h>
 
 #include "LLlex.h"
 #include "Lpars.h"
@@ -64,12 +65,7 @@ GetFile(name) char* name;
 	/*	Try to find a file with basename "name" and extension ".def",
 		in the directories mentioned in "DEFPATH".
 	*/
-	char buf[15];
-	char *strncpy(), *strcat();
-
-	strncpy(buf, name, 10);
-	buf[10] = '\0'; /* maximum length */
-	strcat(buf, ".def");
+	char* buf = aprintf("%s.def", name);
 	DEFPATH[0] = WorkingDir;
 	if (!InsertFile(buf, DEFPATH, &(FileName)))
 	{

lang/m2/comp/build.lua
@@ -113,6 +113,7 @@ cprogram {
 		"h+emheaders",
 		"modules+headers",
 		"modules/src/alloc+lib",
+		"modules/src/data+lib",
 		"modules/src/em_code+lib_k",
 		"modules/src/em_data+lib",
 		"modules/src/em_mes+lib",

commit 229b80a004b1d5498f967f53aa258c7c8944b808

Free buf in GetFile().
aprintf() returns a const char *; the assignment to char * caused both
clang and gcc to warn of the dropped const.

Commit 893471a introduced a tiny memory leak, because GetFile()
stopped freeing buf.  The const return type of aprintf() suggests that
the buffer must not be freed.

Now use Malloc() to allocate the buffer and free() to free it.  This
also checks if we are out of memory, because Malloc() does the check
and aprintf() currently doesn't.

lang/m2/comp/defmodule.c
@@ -17,7 +17,7 @@
 #include <em_arith.h>
 #include <em_label.h>
 #include <stdlib.h>
-#include <astring.h>
+#include <string.h>
 
 #include "LLlex.h"
 #include "Lpars.h"
@@ -59,15 +59,24 @@ char*
 	return "";
 }
 
-STATIC
+STATIC int
 GetFile(name) char* name;
 {
 	/*	Try to find a file with basename "name" and extension ".def",
 		in the directories mentioned in "DEFPATH".
 	*/
-	char* buf = aprintf("%s.def", name);
+	size_t len;
+	int found;
+	char *buf;
+
+	len = strlen(name);
+	buf = Malloc(len + 5);
+	memcpy(buf, name, len);
+	memcpy(buf + len, ".def", 5);
 	DEFPATH[0] = WorkingDir;
-	if (!InsertFile(buf, DEFPATH, &(FileName)))
+	found = InsertFile(buf, DEFPATH, &(FileName));
+	free(buf);
+	if (!found)
 	{
 		error("could not find a DEFINITION MODULE for \"%s\"", name);
 		return 0;