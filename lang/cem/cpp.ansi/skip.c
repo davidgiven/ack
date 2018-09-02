274ed3cb6ab6ce060ed27f535117f45d62fa140d
David Given <dg@cowlark.com>
2018-09-02 11:49:40 +0200

    Run through clang-format before editing.

--- lang/cem/cpp.ansi/skip.c
+++ lang/cem/cpp.ansi/skip.c
@@ -5,53 +5,59 @@
 /* $Id$ */
 /* PREPROCESSOR: INPUT SKIP FUNCTIONS */
 
-#include	"arith.h"
-#include	"LLlex.h"
-#include	"class.h"
-#include	"input.h"
+#include "arith.h"
+#include "LLlex.h"
+#include "class.h"
+#include "input.h"
 
 extern int InputLevel;
 
-int
-skipspaces(ch, skipnl)
-	register int ch;
+int skipspaces(ch, skipnl) register int ch;
 {
 	/*	skipspaces() skips any white space and returns the first
-		non-space character.
+	    non-space character.
 	*/
 	register int nlseen = 0;
 
-	for (;;) {
+	for (;;)
+	{
 		while (class(ch) == STSKIP)
 			ch = GetChar();
-		if (skipnl && class(ch) == STNL) {
+		if (skipnl && class(ch) == STNL)
+		{
 			ch = GetChar();
 			LineNumber++;
 			nlseen++;
 			continue;
 		}
-		if (ch == TOKSEP && InputLevel) {
+		if (ch == TOKSEP && InputLevel)
+		{
 			ch = GetChar();
 			continue;
 		}
 
 		/* \\\n are handled by trigraph */
 
-		if (ch == '/') {
+		if (ch == '/')
+		{
 			ch = GetChar();
-			if (ch == '*' && !InputLevel) {
+			if (ch == '*' && !InputLevel)
+			{
 				skipcomment();
 				ch = GetChar();
 			}
-			else	{
+			else
+			{
 				UnGetChar();
 				return '/';
 			}
 		}
-		else if (nlseen && ch == '#') {
+		else if (nlseen && ch == '#')
+		{
 			domacro();
 			ch = GetChar();
-		} else
+		}
+		else
 			return ch;
 	}
 }
@@ -62,31 +68,45 @@ SkipToNewLine()
 	register int garbage = 0;
 	register int delim = 0;
 
-	while ((ch = GetChar()) != '\n') {
-		if (delim) {
-			if (ch == '\\') {
-				if (GetChar() == '\n') break;
-			} else if (ch == delim) {
+	while ((ch = GetChar()) != '\n')
+	{
+		if (delim)
+		{
+			if (ch == '\\')
+			{
+				if (GetChar() == '\n')
+					break;
+			}
+			else if (ch == delim)
+			{
 				delim = 0;
 			}
 			continue;
-		} else if (ch == '\'' || ch == '\"') {
+		}
+		else if (ch == '\'' || ch == '\"')
+		{
 			delim = ch;
 			garbage = 1;
-		} else if (ch == '/') {
-			if (GetChar() == '*' && !InputLevel) {
+		}
+		else if (ch == '/')
+		{
+			if (GetChar() == '*' && !InputLevel)
+			{
 				skipcomment();
 				continue;
 			}
-			else UnGetChar();
+			else
+				UnGetChar();
 		}
-		else if (ch == TOKSEP && InputLevel) {
+		else if (ch == TOKSEP && InputLevel)
+		{
 			continue;
 		}
 		if (!is_wsp(ch))
 			garbage = 1;
 	}
-	if (delim) strict("unclosed opening %c", delim);
+	if (delim)
+		strict("unclosed opening %c", delim);
 	++LineNumber;
 	return garbage;
 }
