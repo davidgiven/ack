a91ab5c59995342cbe4a1d7d400ebea6660290f0
David Given <dg@cowlark.com>
2019-02-08 21:35:14 +0100

    Somehow the implementation of rst 2 got lost, with hilarious results. Also fix some register corruption issues (I hope).

--- mach/i80/libem/rst.s
+++ mach/i80/libem/rst.s
@@ -9,8 +9,11 @@
 .rst_init:
     mvi a, 0xc3     ! jmp <a16>
     sta 0x08
+    sta 0x10
     lxi h, rst1
     shld 0x09
+    lxi h, rst2
+    shld 0x11
     ret
 
     ! de = [bc+const1] (remember bc is the frame pointer)
@@ -30,3 +33,22 @@ rst1:
     inx h
     mov d, m
     ret
+
+    ! [bc+const1] = de (remember bc is the frame pointer)
+rst2:
+    pop h
+    mov a, m
+    inx h
+    push h
+
+	mov l, a
+	ral
+	sbb a
+	mov h, a
+
+	dad b
+    mov m, e
+    inx h
+    mov m, d
+    ret
+
