813595358ce0c478e2845b43d38665cb257f19c7
David Given <dg@cowlark.com>
2019-02-10 14:09:52 +0100

    More common symbol removal.

--- util/int/global.c
+++ util/int/global.c
@@ -10,61 +10,61 @@
 
 /******** EM Machine capacity parameters ********/
 
-size wsize;
-size dwsize;
-size psize;
-long i_minsw;
-long i_maxsw;
-unsigned long i_maxuw;
-long min_off;
-long max_off;
-ptr max_addr;
-size wsizem1;
+size wsize = 0;
+size dwsize = 0;
+size psize = 0;
+long i_minsw = 0;
+long i_maxsw = 0;
+unsigned long i_maxuw = 0;
+long min_off = 0;
+long max_off = 0;
+ptr max_addr = 0;
+size wsizem1 = 0;
 
 
 /******** EM program parameters ********/
 
-ptr ML;
-ptr HB;
-ptr DB;
-long NProc;
-long PreIgnMask;
+ptr ML = 0;
+ptr HB = 0;
+ptr DB = 0;
+long NProc = 0;
+long PreIgnMask = 0;
 
 
 /******** EM machine registers ********/
 
 
-long PI;
-ptr PC;
+long PI = 0;
+ptr PC = 0;
 
-ptr HP;
-ptr SP;
-ptr LB;
-ptr AB;
+ptr HP = 0;
+ptr SP = 0;
+ptr LB = 0;
+ptr AB = 0;
 
-long ES;
-int ES_def;
+long ES = 0;
+int ES_def = 0;
 
-int OnTrap;
-long IgnMask;
-long TrapPI;
+int OnTrap = 0;
+long IgnMask = 0;
+long TrapPI = 0;
 
-char *FRA;
-size FRALimit;
-size FRASize;
-int FRA_def;
+char *FRA = NULL;
+size FRALimit = 0;
+size FRASize = 0;
+int FRA_def = 0;
 
 
 /******** The EM Machine Memory ********/
 
-char *text;
+char *text = NULL;
 
-char *data;
-ptr HL;
+char *data = NULL;
+ptr HL = 0;
 
-char *stack;
-char *stackML;
-ptr SL;
+char *stack = NULL;
+char *stackML = NULL;
+ptr SL = 0;
 
 
 
