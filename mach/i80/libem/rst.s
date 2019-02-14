91c75f81b54c2a808ff115851295d7898e47043b
David Given <dg@cowlark.com>
2019-02-15 00:38:39 +0100

    Call cmps rather than cmpu for bge... make cmps and cmpu rsts. Apply various other tiny optimisations.

--- mach/i80/libem/rst.s
+++ mach/i80/libem/rst.s
@@ -11,6 +11,10 @@
 !     41 call .floadn4
 !     34 call .fload4
 !     28 call .fstoren2
+!
+! Also:
+!     48 call .cmps_mag
+!     25 call .cmpu_mag
 
 .define .rst_init
 .rst_init:
@@ -18,13 +22,14 @@
     lxi d, 0x0008
     call copy
     lxi h, .floadn4
-    lxi d, 0x0010
     call copy
     lxi h, .fload4
-    lxi d, 0x0018
     call copy
     lxi h, .fstoren2
-    lxi d, 0x0020
+    call copy
+    lxi h, .cmps_mag
+    call copy
+    lxi h, .cmpu_mag
     jmp copy
 
 ! Copies eight bytes from HL to DE.
