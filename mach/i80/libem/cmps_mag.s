2924ce8ffae7a9a9e2adacfd015e25d564af3dbe
David Given <dg@cowlark.com>
2019-02-14 23:57:55 +0100

    Use nicely optimised helper tools to do signed comparisons of words. Star Trek goes from 40452 to 39890 bytes.

---
+++ mach/i80/libem/cmps_mag.s
@@ -0,0 +1,18 @@
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+.sect .text
+
+! Does a tristate signed comparison of hl <> de.
+! Returns m flag if hl < de.
+! Returns p flag if hl >= de.
+! This doesn't set z coherently.
+
+.define .cmps_mag
+.cmps_mag:
+    mov a, d
+    xra h
+    jp .cmpu_mag ! signs are the same, so an unsigned comparison will do
+    xra h        ! set A=H and set the sign flag
+    ret
---
+++ mach/i80/libem/cmpu_mag.s
@@ -0,0 +1,19 @@
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+.sect .text
+
+! Does a tristate unsigned comparison of hl <> de.
+! Returns m flag if hl < de.
+! Returns p flag if hl >= de.
+! This doesn't set z coherently.
+
+.define .cmpu_mag
+.cmpu_mag:
+    mov a, e
+    sub l
+    mov a, d
+    sbb h
+    rar
+    ret
