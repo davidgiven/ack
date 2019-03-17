8b525f59c38ee52e806d46bc416d8e8ed4097ebc
carl <cecodere@yahoo.ca>
2019-03-17 22:42:20 +0800

    + ANSI C conversion

---
+++ util/int/data.h
@@ -0,0 +1,30 @@
+/*  Copyright (c) 2019 ACK Project.
+ *  See the copyright notice in the ACK home directory, 
+ *  in the file "Copyright".
+ *
+ */
+#ifndef DATA_H_
+#define DATA_H_
+
+#include "global.h"
+
+
+void init_data(ptr hb);
+void newHP(ptr ap);
+void dt_stdp(register ptr addr, ptr ap);
+void dt_stn(register ptr addr, long al, size n);
+void dt_stw(register ptr addr, long al);
+void dt_stip(register ptr addr, ptr ap);
+#ifndef	NOFLOAT
+void dt_stf(register ptr addr, double f, register size n);
+#endif
+
+ptr dt_lddp(register ptr addr);
+unsigned long dt_ldu(register ptr addr, size n);
+unsigned long dt_lduw(register ptr addr);
+long dt_lds(register ptr addr, size n);
+long dt_ldsw(register ptr addr);
+void dt_mvd(ptr d2, ptr d1, size n);
+void dt_mvs(ptr d, ptr s, size n);
+
+#endif /* DATA_H_ */
---
+++ util/int/dump.h
@@ -0,0 +1,17 @@
+/*  Copyright (c) 2019 ACK Project.
+ *  See the copyright notice in the ACK home directory, 
+ *  in the file "Copyright".
+ *
+ *  Created on: 2019-03-16
+ *  
+ */
+#ifndef DUMP_H_
+#define DUMP_H_
+
+#ifdef LOGGING
+void std_all(long, int);
+void gdad_all(ptr, ptr);
+void hpd_all(void);
+#endif
+
+#endif /* DUMP_H_ */
