9bb69bbb9890c104a8cbd803cc487c0f0bfd1ff5
carl <cecodere@yahoo.ca>
2019-05-14 23:21:19 +0800

    Fix several compiler warnings by adding braces

--- mach/proto/as/comm3.c
+++ mach/proto/as/comm3.c
@@ -23,30 +23,30 @@ struct outhead	outhead = {
 #include	"y.tab.h"
 
 item_t	keytab[] = {
-	0,	EXTERN,		0,		".define",
-	0,	EXTERN,		0,		".extern",
-	0,	DOT,		0,		".",
-	0,	DATA,		RELO1,	".data1",
-	0,	DATA,		RELO2,	".data2",
-	0,	DATA,		RELO4,	".data4",
-	0,  DATAF,      4,      ".dataf4",
-	0,  DATAF,      8,      ".dataf8",
-	0,	ASCII,		0,		".ascii",
-	0,	ASCII,		1,		".asciz",
-	0,	ALIGN,		0,		".align",
-	0,	ASSERT,		0,		".assert",
-	0,	SPACE,		0,		".space",
-	0,	COMMON,		0,		".comm",
-	0,	SECTION,	0,		".sect",
-	0,	BASE,		0,		".base",
-	0,	SYMB,		0,		".symb",
-	0,	SYMD,		0,		".symd",
-	0,	LINE,		0,		".line",
-	0,	FILe,		0,		".file",
+	{0,	EXTERN,		0,		".define"},
+	{0,	EXTERN,		0,		".extern"},
+	{0,	DOT,		0,		"."},
+	{0,	DATA,		RELO1,	".data1"},
+	{0,	DATA,		RELO2,	".data2"},
+	{0,	DATA,		RELO4,	".data4"},
+	{0,  DATAF,      4,      ".dataf4"},
+	{0,  DATAF,      8,      ".dataf8"},
+	{0,	ASCII,		0,		".ascii"},
+	{0,	ASCII,		1,		".asciz"},
+	{0,	ALIGN,		0,		".align"},
+	{0,	ASSERT,		0,		".assert"},
+	{0,	SPACE,		0,		".space"},
+	{0,	COMMON,		0,		".comm"},
+	{0,	SECTION,	0,		".sect"},
+	{0,	BASE,		0,		".base"},
+	{0,	SYMB,		0,		".symb"},
+	{0,	SYMD,		0,		".symd"},
+	{0,	LINE,		0,		".line"},
+	{0,	FILe,		0,		".file"},
 #ifdef LISTING
-	0,	LIST,		0,		".nolist",
-	0,	LIST,		1,		".list",
+	{0,	LIST,		0,		".nolist"},
+	{0,	LIST,		1,		".list"},
 #endif
 #include	"mach3.c"
-	0,	0,		0,		0
+	{0,	0,		0,		0}
 };
