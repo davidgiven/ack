f0a2c84d9358353d97bc92bb47ec702652033461
George Koehler <kernigh@gmail.com>
2019-09-18 14:09:51 -0400

    Tweak i386 rules for adi 8, sli 8, sru 8.
    Add EXACT to the rule for adi 8, in the same way that the old rules
    for and 8, ior 8, xor 8 have EXACT.
    
    Add rules for sli 8 and sru 8 when shifting 32 bits, and add
    assertions in llshift_e.c to test these rules.

--- tests/plat/long-long/llshift_e.c
+++ tests/plat/long-long/llshift_e.c
@@ -17,6 +17,8 @@ void _m_a_i_n(void) {
 	ASSERT(i << (1 + zero) == 242LL);
 	ASSERT(i << 26 == 8120172544LL);
 	ASSERT(i << (26 + zero) == 8120172544LL);
+	ASSERT(i << 32 == 519691042816LL);
+	ASSERT(i << (32 + zero) == 519691042816LL);
 	ASSERT(i << 56 == 8718968878589280256LL);
 	ASSERT(i << (56 + zero) == 8718968878589280256LL);
 
@@ -67,9 +69,10 @@ void _m_a_i_n(void) {
 	ASSERT(u >> (0 + zero) == 12022195707510591570ULL);
 	ASSERT(u >> 1 == 6011097853755295785ULL);
 	ASSERT(u >> (1 + zero) == 6011097853755295785ULL);
+	ASSERT(u >> 32 == 2799135564ULL);
+	ASSERT(u >> (32 + zero) == 2799135564ULL);
 	ASSERT(u >> 41 == 5467061ULL);
 	ASSERT(u >> (41 + zero) == 5467061ULL);
 
 	finished();
 }
-
