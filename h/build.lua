f5acf7091110a96aee070e52574827cd06b3a8cc
David Given <dg@cowlark.com>
2022-07-20 00:24:52 +0200

    Typo fix.

--- h/build.lua
+++ h/build.lua
@@ -3,7 +3,7 @@ normalrule {
 	ins = {},
 	outleaves = { "em_path.h" },
 	commands = {
-		"echo '#define EM_DIR \"$(PREFIX)\"' >> %{outs}",
+		"echo '#define EM_DIR \"$(PREFIX)\"' > %{outs}",
 		"echo '#define ACK_PATH \"share/ack/descr\"' >> %{outs}",
 	}
 }
