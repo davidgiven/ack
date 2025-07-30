e591ef8c133befd27e43f2e7f74d46994f6bf63e
David Given <dg@cowlark.com>
2025-07-30 21:37:51 +0100

    Format.

--- util/ass/asscm.h
+++ util/ass/asscm.h
@@ -1,9 +1,9 @@
 /*  Copyright (c) 2019 ACK Project.
- *  See the copyright notice in the ACK home directory, 
+ *  See the copyright notice in the ACK home directory,
  *  in the file "Copyright".
  *
  *  Created on: 2019-03-12
- *  
+ *
  */
 #ifndef ASSCM_H_
 #define ASSCM_H_
